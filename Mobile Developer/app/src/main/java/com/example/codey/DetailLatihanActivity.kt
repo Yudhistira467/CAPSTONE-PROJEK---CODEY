c3b583db58aefe388aeb4c93ab514b9f680572da
Alam12346 <syukursidiqnuralam@gmail.com>
2024-11-23 13:47:28 +0700

    add content

---
+++ Mobile Developer/app/src/main/java/com/example/codey/DetailLatihanActivity.kt
@@ -0,0 +1,20 @@
+package com.example.codey
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DetailLatihanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detail_latihan)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
