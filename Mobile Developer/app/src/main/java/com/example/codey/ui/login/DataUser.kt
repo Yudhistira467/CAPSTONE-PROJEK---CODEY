419c5726def84ad39d70f1d0717fd6cd459b737f
nauramufidh <nauramufidah274@gmail.com>
2024-11-25 18:02:06 +0700

    undo

---
+++ Mobile Developer/app/src/main/java/com/example/codey/ui/login/DataUser.kt
@@ -0,0 +1,8 @@
+package com.example.codey.ui.login
+
+data class DataUser(
+    val id: String? = null,
+    val username: String? = null,
+    val email: String? = null,
+    val password: String? = null
+)
